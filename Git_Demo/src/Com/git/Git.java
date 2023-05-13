95ecab6de3bd5bd60d170f7f7b480c09b92112d9
yogitamsawant <133479967+yogitamsawant@users.noreply.github.com>
2023-05-12 23:12:48 -0700

    Update Git.java

--- Git_Demo/src/Com/git/Git.java
+++ Git_Demo/src/Com/git/Git.java
@@ -2,11 +2,14 @@ package Com.git;
 
 public class Git 
 {
-
 	public static void main(String[] args)
 	{
 		System.out.println("<<<Hello GIT>>>");
 
+	}
+	public void m1()
+	{
+		System.out.println("<<<Hello GIT>>>");
 	}
 
 }
